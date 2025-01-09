f186a71a0bd089ac516b6bbc0373c10c21388f9b
MOUSTAFA HAYDAR <programmermoustafa@gmail.com>
2025-01-09 13:28:35 +0200

    Signin page created

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+let home = document.getElementById("home-btn")
+let adopt = document.getElementById("adopt-btn")
+let shop = document.getElementById("shop-btn")
+let volunteer = document.getElementById("volunteer-btn")
+let whoWeAre = document.getElementById("who-we-are-btn")
+let signin = document.getElementById("sign-in-btn")
+
+
+home.addEventListener("click", function() {
+    window.location.href = "index.html";
+})
+
+adopt.addEventListener("click", function() {
+    window.location.href = "adopt.html";
+})
+
+shop.addEventListener("click", function() {
+    window.location.href = "shop.html";
+})
+
+volunteer.addEventListener("click", function() {
+    window.location.href = "volunteer.html";
+})
+
+whoWeAre.addEventListener("click", function() {
+    window.location.href = "whoWeAre.html";
+})
+
+signin.addEventListener("click", function() {
+    window.location.href = "signin.html";
+})
