40bdeea6fdfb833ff2990ec7ce1d265eade04ae2
Lama <158574898+code0lama@users.noreply.github.com>
2025-01-10 19:10:24 +0200

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -1,22 +1,32 @@
 <?php
+require_once('connection.php');
 
-require 'connection.php';
-
-if (isset($_POST["submit"])) {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-    $query = "INSERT INTO USER (name, email, passsword) VALUES('$name', '$email', '$password')";
-
-    if (mysqli_query($conn, $query)) {
-        echo "New record created successfully.";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name']; 
+    $email = $_POST['email']; 
+    $password = $_POST['password']; 
+    
+   
+    $check_email_sql = "SELECT * FROM users WHERE email='$email'";
+    $check_email_result = $conn->query($check_email_sql);
+    
+    if ($check_email_result->num_rows > 0) {
+       
+        echo "<script>alert('Email already exists. Please choose a different email.'); window.location='../Pages/signup.html';</script>";
+        exit;
     } else {
-        echo "Error: " . $query . mysqli_error($conn);
+    
+        $insert_sql = "INSERT INTO users (name,email, password) VALUES ('$name', '$email', '$password')";
+        
+        if ($conn->query($insert_sql) === TRUE) {
+            echo "<script>alert('You have successfully registered. Redirecting you to the login page.'); window.location='../Pages/signin.html';</script>";
+            exit;
+        } else {
+            echo "Error: " . $insert_sql . "<br>" . $conn->error;
+        }
     }
-
+    
+    $conn->close();
 }
-
-
 ?>
+
