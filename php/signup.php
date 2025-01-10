8dec101dacc7bf8d51dbca19d0cbaa95a92c9f82
MOUSTAFA HAYDAR <programmermoustafa@gmail.com>
2025-01-10 18:40:02 +0200

    Signup.php implemented

--- php/signup.php
+++ php/signup.php
@@ -6,10 +6,15 @@ if (isset($_POST["submit"])) {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $password = $_POST["password"];
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $query = "INSERT INTO USER VALUES($name, $email, $password)";
-    mysqli_query($conn, $query);
+    $query = "INSERT INTO USER (name, email, passsword) VALUES('$name', '$email', '$password')";
 
+    if (mysqli_query($conn, $query)) {
+        echo "New record created successfully.";
+    } else {
+        echo "Error: " . $query . mysqli_error($conn);
+    }
 
 }
 
